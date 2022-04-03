b2548a07a42d733cb8280a1bfe91b8c6b85df542
Volodymyr Holub <volodymyr.hollub@gmail.com>
2022-04-03 19:09:13 +0300

    added docker-compose.yml and added .env file

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -3,9 +3,9 @@ import { ConnectionOptions } from 'typeorm';
 const config: ConnectionOptions = {
   type: 'postgres',
   host: 'localhost',
-  username: 'postgres',
-  password: 'postgres',
-  database: 'mediumclone',
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: false,
   migrations: [__dirname + '/migrations/**/*{.ts, .js}'],
